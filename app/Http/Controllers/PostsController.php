0ac0b11d7a371152b0faf2e25e6c29d8728e1f6e
test <test@gmail.com>
2018-11-04 02:13:19 +0000

    test

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index(){
+        //$posts = \App\Post::all();
+        //$posts = Post::all();
+        //$posts = Post::orderBy('created_at','desc')->get();
+        $posts = Post::latest()->get();
+        //$posts =[];
+        //dd($posts->toArray()); //dump die
+        //return view('posts.index',['posts => $posts']);
+        return view('posts.index')->with('posts',$posts);
+         }
+
+    public function show($id){
+       // $post = Post::find($id);
+        $post = Post::findOrFail($id);
+        return view('posts.index')->with('posts',$posts);
+    }
+    
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang ="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Blog Posts</title>
+    <link rel ="stylesheet"href="/css/styles.css">
+    
+</head>
+<body>
+    <div class="container">
+        <h1>Blog Posts</h1>
+            <ul>
+                {{--
+                @foreach($posts as $post)
+                <li><a href="">{{ $posts->title }}</a></li>
+                @endforeach
+                --}}
+                @forelse ($posts as $post)
+                 <li><a href="">{{ $post->title }}</a></li>
+                @empty
+                <li>No posts yet</li>
+                @endforelse
+            </ul>
+    </div>
+</body>
+</html>
